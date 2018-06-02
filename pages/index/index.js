31634455f9e1172125f1908a10fa8b150b7fa5a9
Benjamin hang <406227036@qq.com>
2018-06-02 12:59:06 +0800

    更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -61,6 +61,17 @@ Page({
         hasUserInfo: true,
         avatarUrl: app.globalData.userInfo.avatarUrl
       })
+      wx.downloadFile({
+        url: res.userInfo.avatarUrl,
+        success: resTemp => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+            avatarUrl: resTemp.tempFilePath
+          })
+        }
+      })
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
